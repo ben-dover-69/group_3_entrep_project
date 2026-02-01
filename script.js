451829410d5534aa2c0cebd37c2155fa2bc5dd23
ben-dover-69 <kingrayethb@gmail.com>
2026-02-01 14:53:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function sendMail(){
+let parms = {
+  
+     name : document. getElementById ("name"). value, 
+     email: document. getElementById("email"). value,
+     subject: document.getElementById("subject"). value,
+     message : document. getElementById("message"). value,
+}
+  emailjs.send("service_blwhkvs","template_ka72mdg",parms).then(alert("Email Sent!"))
+    }
